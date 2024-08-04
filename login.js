9036b21029a49d31c9ab8d035a57f17104143c8f
Yash Bisht <yashbisht2005@gmail.com>
2024-08-04 12:37:53 +0530

    signup popup updated

---
+++ login.js
@@ -0,0 +1,58 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCVdX-U__-6zngFiJ_rAdcC9yErXBzhJ5Y",
+  authDomain: "crispycrusts-2269b.firebaseapp.com",
+  projectId: "crispycrusts-2269b",
+  storageBucket: "crispycrusts-2269b.appspot.com",
+  messagingSenderId: "875783626253",
+  appId: "1:875783626253:web:8d9706f372e1b93ac17f44"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+//function to validate email
+function isValidEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+
+//submit
+const submit = document.getElementById("submit");
+submit.addEventListener("click", function(event){
+  event.preventDefault();
+
+  const email = document.getElementById("email").value.trim();
+  const password = document.getElementById("create-pwd").value;
+
+  if(!isValidEmail(email)) {
+    alert("Please enter a valid email address.");
+    return;
+  }
+
+  if(password.length < 6) {
+      alert("Password must be at least 6 characters long.");
+      return;
+  }
+
+  signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed up 
+    const user = userCredential.user;
+    alert("Creating Account...");
+    window.location.href = "index.html"
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    alert(errorMessage);
+});
+});
+
--- signup.js
+++ signup.js
@@ -2,6 +2,8 @@ const loginbtn = document.querySelector(".login");
 const signupbtn = document.querySelector(".Signup");
 const form1 = document.querySelector(".form1");
 const form2 = document.querySelector(".form2");
+    
+
 
 loginbtn.addEventListener("click",() => {
     form1.style.display = "none";
@@ -13,3 +15,21 @@ signupbtn.addEventListener("click",() => {
     form1.style.display = "";
 
 })
+
+// signupbutton.addEventListener ( "click", () => {
+//     signuppopup.style.display = "" ;
+//     // header.style.display = "none";
+//     // tittle.style.display = "none";
+//     // signupbutton.style.display = "none";
+//     // frontpg.classList.add("show");
+//     // // frontpg.style.filter = "brightness(70%)";
+//     // // signuppopup.style.filter = "brightness(150%);"
+
+// });
+
+function toggle(){
+    var blur = document.querySelector("#blur");
+    blur.classList.toggle("active");
+    var popup = document.querySelector("#Signup-box");
+    popup.classList.toggle("active")
+}
